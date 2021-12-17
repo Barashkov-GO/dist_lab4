ae24c9b1f13c31fc8f5edba313a5048d6cedc694
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-17 23:16:48 +0300

    Scripted auto-commit on change (2021-12-17 23:16:48) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/TestPackage.java
+++ src/main/java/ru.barashkov.distributed.lab4/TestPackage.java
@@ -1,9 +1,11 @@
 package ru.barashkov.distributed.lab4;
 
+import java.util.ArrayList;
+
 public class TestPackage {
     private final Integer packageId;
     private final String jscript;
     private final String funcName;
-    private final 
+    private final ArrayList<Test> 
 
 }

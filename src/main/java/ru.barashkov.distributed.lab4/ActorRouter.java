fc3f1405722cdb0dac7e0efdf76ee92ad5bb7fe8
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-18 00:21:54 +0300

    Scripted auto-commit on change (2021-12-18 00:21:54) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/ActorRouter.java
+++ src/main/java/ru.barashkov.distributed.lab4/ActorRouter.java
@@ -4,16 +4,20 @@ import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.Props;
 
+import java.util.ArrayList;
+
 public class ActorRouter extends AbstractActor {
     private static final Integer TEST_ACTORS_AMOUNT = 10;
     private final ActorRef storageActor;
 
 
     public ActorRouter() {
+        ArrayList<ActorRef> actors = new ArrayList<>();
         this.storageActor = getContext().actorOf(Props.create(ActorStorage.class));
         for (int i = 0; i < TEST_ACTORS_AMOUNT; i++) {
-            ActorRef ac = getContext().actorOf(Props.create(ActorTest.class));
+            actors.add(getContext().actorOf(Props.create(ActorTest.class)));
         }
+        
 
     }
     @Override

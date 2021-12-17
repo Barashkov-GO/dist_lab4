09027c12976c49fe973c9a86f858bc4e80f380a5
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-18 00:20:09 +0300

    Scripted auto-commit on change (2021-12-18 00:20:09) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/ActorRouter.java
+++ src/main/java/ru.barashkov.distributed.lab4/ActorRouter.java
@@ -11,7 +11,9 @@ public class ActorRouter extends AbstractActor {
 
     public ActorRouter() {
         this.storageActor = getContext().actorOf(Props.create(ActorStorage.class));
-        
+        for (int i = 0; i < TEST_ACTORS_AMOUNT; i++) {
+            
+        }
 
     }
     @Override

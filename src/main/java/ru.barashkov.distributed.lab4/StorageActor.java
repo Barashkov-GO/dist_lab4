8465bf6b6f784165406e9c091ed7f5025ee48aad
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-17 20:41:02 +0300

    Scripted auto-commit on change (2021-12-17 20:41:02) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/StorageActor.java
+++ src/main/java/ru.barashkov.distributed.lab4/StorageActor.java
@@ -31,7 +31,7 @@ public class StorageActor extends AbstractActor{
     }
 
     private void setResult(MessageSetResult m) {
-        String Id = m.getId();
+        String Id = (String) m.getId();
         if (storage.containsKey(m.getId())) {
             storage.get().
                     add(m.getResult());

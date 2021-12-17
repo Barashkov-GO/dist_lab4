66ae162586f83fc6469128e5c7542b0557bd8980
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-17 20:36:51 +0300

    Scripted auto-commit on change (2021-12-17 20:36:51) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/StorageActor.java
+++ src/main/java/ru.barashkov.distributed.lab4/StorageActor.java
@@ -14,7 +14,7 @@ public class StorageActor extends AbstractActor{
     public Receive createReceive() {
         return ReceiveBuilder.create().
                 match(MessageSetResult.class, m -> setResult(m)).
-                match(MessageGetResult.class, m -> sender().tell().
+                match(MessageGetResult.class, m -> sender().tell(new Mess).
                 build();
     }
 

7d3a0508456beca8640c19cd4ce5ae99addf47a1
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-17 20:07:09 +0300

    Scripted auto-commit on change (2021-12-17 20:07:09) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/StorageActor.java
+++ src/main/java/ru.barashkov.distributed.lab4/StorageActor.java
@@ -5,7 +5,7 @@ import akka.japi.pf.ReceiveBuilder;
 
 public class StorageActor extends AbstractActor{
 
-    private Map<Integer, >
+    private Map<Integer, List<>>
     @Override
     public Receive createReceive() {
         return null;

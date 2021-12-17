3722d545e8c81e616405ae4daa20f4c050dd0078
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-17 21:04:06 +0300

    Scripted auto-commit on change (2021-12-17 21:04:06) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/TestResult.java
+++ src/main/java/ru.barashkov.distributed.lab4/TestResult.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class TestResult {
     private final String testName;
     private final String expectedResult;
-    
+    private final String 
     public TestResult(String id, ArrayList<TestResult> testResults) {
     }
 }

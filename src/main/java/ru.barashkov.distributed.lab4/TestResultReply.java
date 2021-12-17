f61861d27b0c922f7c763d4dba1e081821727823
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-17 21:01:02 +0300

    Scripted auto-commit on change (2021-12-17 21:01:02) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/TestResultReply.java
+++ src/main/java/ru.barashkov.distributed.lab4/TestResultReply.java
@@ -1,8 +1,12 @@
 package ru.barashkov.distributed.lab4;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.ArrayList;
 
 public class TestResultReply {
-    public TestResultReply(String id, ArrayList<TestResult> testResults) {
+    public TestResultReply(
+            @JsonProperty("packageId") String packageId,
+            @JsonProperty("results") ArrayList<TestResult> testResults) {
     }
 }

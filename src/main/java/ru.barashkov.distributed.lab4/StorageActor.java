b2b506cbb39cd00781ae42152e35b4b535755484
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-17 20:09:47 +0300

    Scripted auto-commit on change (2021-12-17 20:09:47) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/StorageActor.java
+++ src/main/java/ru.barashkov.distributed.lab4/StorageActor.java
@@ -3,9 +3,11 @@ package ru.barashkov.distributed.lab4;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.HashMap;
+
 public class StorageActor extends AbstractActor{
 
-    private Map<Integer, ArrayList<TestResult>>
+    private Map<Integer, ArrayList<TestResult>> storage = new HashMap<>()
     @Override
     public Receive createReceive() {
         return null;

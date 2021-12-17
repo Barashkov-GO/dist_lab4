6d2b03b73776fc09fb096694b252a17919348ed7
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-17 20:31:32 +0300

    Scripted auto-commit on change (2021-12-17 20:31:32) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/MessageGetResult.java
+++ src/main/java/ru.barashkov.distributed.lab4/MessageGetResult.java
@@ -2,7 +2,7 @@ package ru.barashkov.distributed.lab4;
 
 public class MessageGetResult {
     private final String Id;
-    private final 
+    private final ArrayList<TestResult>
     public Object getResult() {
     }
 }

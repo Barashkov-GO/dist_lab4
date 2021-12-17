ce4740ae6aa82b3087704783886adbc8266fd3ab
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-17 20:56:42 +0300

    Scripted auto-commit on change (2021-12-17 20:56:42) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/MessageSetResult.java
+++ src/main/java/ru.barashkov.distributed.lab4/MessageSetResult.java
@@ -1,9 +1,25 @@
 package ru.barashkov.distributed.lab4;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+
 public class MessageSetResult {
-    public Object getId() {
+    private final String Id;
+    private final ArrayList<TestResult> testResults;
+
+    public MessageSetResult(
+            @JsonProperty("packageId") String packageId,
+            @JsonProperty("results") ArrayList<TestResult> testResults) {
+        this.Id = packageId;
+        this.testResults = testResults;
     }
 
-    public Object getResult() {
+    public String getId() {
+        return this.Id;
+    }
+
+    public ArrayList<TestResult> getResult() {
+        return this.testResults;
     }
 }

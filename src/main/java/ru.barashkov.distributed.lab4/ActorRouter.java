5c53c5ab2f9c3863d6ce0e591cc53e250ff424ea
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-18 00:24:31 +0300

    Scripted auto-commit on change (2021-12-18 00:24:31) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/ActorRouter.java
+++ src/main/java/ru.barashkov.distributed.lab4/ActorRouter.java
@@ -28,7 +28,8 @@ public class ActorRouter extends AbstractActor {
 
     @Override
     public Receive createReceive() {
-        return receiveBuilder().build()
+        return receiveBuilder().
+                match()
 
     }
 }

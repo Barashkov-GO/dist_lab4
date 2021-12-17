eca9226d3b1b958ca807bd042a9bf3d9775607b4
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-18 00:23:48 +0300

    Scripted auto-commit on change (2021-12-18 00:23:48) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/ActorRouter.java
+++ src/main/java/ru.barashkov.distributed.lab4/ActorRouter.java
@@ -5,24 +5,27 @@ import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.routing.BalancingRoutingLogic;
 import akka.routing.RoutedActorCell;
+import akka.routing.Routee;
 import akka.routing.Router;
 
 import java.util.ArrayList;
 
 public class ActorRouter extends AbstractActor {
     private static final Integer TEST_ACTORS_AMOUNT = 10;
+
     private final ActorRef storageActor;
+    private final Router router;
 
 
     public ActorRouter() {
-        ArrayList<ActorRef> actors = new ArrayList<>();
+        ArrayList<Routee> actors = new ArrayList<>();
         this.storageActor = getContext().actorOf(Props.create(ActorStorage.class));
         for (int i = 0; i < TEST_ACTORS_AMOUNT; i++) {
             actors.add(getContext().actorOf(Props.create(ActorTest.class)));
         }
-        router = new Router(new BalancingRoutingLogic())
-
+        router = new Router(new BalancingRoutingLogic(), actors);
     }
+    
     @Override
     public Receive createReceive() {
 

3db5f50c1459c8c3e679e8857156729e8a93cbb1
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-18 00:28:03 +0300

    Scripted auto-commit on change (2021-12-18 00:28:03) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/ActorRouter.java
+++ src/main/java/ru.barashkov.distributed.lab4/ActorRouter.java
@@ -4,7 +4,6 @@ import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.routing.BalancingRoutingLogic;
-import akka.routing.RoutedActorCell;
 import akka.routing.Routee;
 import akka.routing.Router;
 
@@ -13,12 +12,12 @@ import java.util.ArrayList;
 public class ActorRouter extends AbstractActor {
     private static final Integer TEST_ACTORS_AMOUNT = 10;
 
-    private final ActorRef storageActor;
+    private final ActorRef storageActorRef;
     private final Router router;
 
 
     public ActorRouter() {
-        this.storageActor = getContext().actorOf(Props.create(ActorStorage.class));
+        this.storageActorRef = getContext().actorOf(Props.create(ActorStorage.class));
         ArrayList<Routee> actors = new ArrayList<>();
         for (int i = 0; i < TEST_ACTORS_AMOUNT; i++) {
             actors.add(getContext().actorOf(Props.create(ActorTest.class)));
@@ -31,7 +30,7 @@ public class ActorRouter extends AbstractActor {
         return receiveBuilder().
                 match(
                         MessageGetResult.class,
-                        m -> 
+                        m -> storageActorRef.tell()
                 ).
                 match(
 

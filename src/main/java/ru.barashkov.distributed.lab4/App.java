39aeb81915b2c364b57f5c9feba4af9d02e0fe06
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-18 00:10:38 +0300

    Scripted auto-commit on change (2021-12-18 00:10:38) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/App.java
+++ src/main/java/ru.barashkov.distributed.lab4/App.java
@@ -1,8 +1,18 @@
 package ru.barashkov.distributed.lab4;
 
+import akka.actor.Props;
+import akka.routing.RoundRobinPool;
+
+import static java.security.AccessController.getContext;
+
 public class App {
 
     public void main(String[] args) {
-        
+        testPerformerActor = getContext().actorOf(
+                new RoundRobinPool(5)
+                        .withSupervisorStrategy(strategy)
+                        .props(Props.create(TestPerformerActor.class, logResultsActor)),
+                "routerForTests"
+        );
     }
 }

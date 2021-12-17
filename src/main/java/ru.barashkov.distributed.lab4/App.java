692ffaf5280cc09fb5fc22a5c75e1c1f1c735005
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-18 00:09:19 +0300

    Scripted auto-commit on change (2021-12-18 00:09:19) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/App.java
+++ src/main/java/ru.barashkov.distributed.lab4/App.java
@@ -1,51 +1,8 @@
 package ru.barashkov.distributed.lab4;
 
-import akka.actor.AbstractActor;
-import akka.japi.pf.ReceiveBuilder;
+public class App {
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
-public class ActorSystem extends AbstractActor{
-
-    private final Map<Integer, ArrayList<TestResult>> storage = new HashMap<>();
-
-    @Override
-    public Receive createReceive() {
-        return ReceiveBuilder.create().
-                match(
-                        MessageSetResult.class,
-                        this::setResult
-                ).
-                match(
-                        MessageGetResult.class,
-                        m -> sender().tell(
-                                new TestResultReply(
-                                        m.getId(),
-                                        storage.get(m.getId())
-                                ),
-                                self()
-                        )
-                ).
-                build();
-    }
-
-    private void setResult(MessageSetResult m) {
-        Integer packageId = m.getPackageId();
-        TestResult testResult = m.getResult();
-
-        if (storage.containsKey(packageId)) {
-            storage.get(packageId).
-                    add(testResult);
-        } else {
-            storage.put(
-                    packageId,
-                    new ArrayList<>(
-                            Collections.singleton(testResult)
-                    )
-            );
-        }
+    public void main(String[] args) {
+        
     }
 }

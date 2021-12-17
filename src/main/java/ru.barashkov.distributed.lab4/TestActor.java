d0093f58fb8c6abc5765022140669dd60efd8cdc
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-17 21:30:32 +0300

    Scripted auto-commit on change (2021-12-17 21:30:32) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/TestActor.java
+++ src/main/java/ru.barashkov.distributed.lab4/TestActor.java
@@ -2,6 +2,10 @@ package ru.barashkov.distributed.lab4;
 
 import akka.actor.AbstractActor;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 public class TestActor extends AbstractActor {
 
     @Override

bbda38abfab87e1c7ac1dda48d56664b0c69e8b6
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-17 23:02:57 +0300

    Scripted auto-commit on change (2021-12-17 23:02:57) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/TestActor.java
+++ src/main/java/ru.barashkov.distributed.lab4/TestActor.java
@@ -14,6 +14,7 @@ public class TestActor extends AbstractActor {
 
     @Override
     public Receive createReceive() {
+        
     }
 
     private String executeJS(String jscript, String functionName, Object[] params)

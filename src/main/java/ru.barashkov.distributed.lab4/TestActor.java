18feb01dd74aef36035365592b2c65e724fa6a0f
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-17 21:29:56 +0300

    Scripted auto-commit on change (2021-12-17 21:29:56) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/TestActor.java
+++ src/main/java/ru.barashkov.distributed.lab4/TestActor.java
@@ -1,11 +1,39 @@
 package ru.barashkov.distributed.lab4;
 
 import akka.actor.AbstractActor;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
 
 public class TestActor extends AbstractActor {
 
     @Override
     public Receive createReceive() {
-        
+        package ru.barashkov.distributed.lab4;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+
+        public class MessageSetResult {
+            private final String Id;
+            private final ArrayList<TestResult> testResults;
+
+            public MessageSetResult(
+                    @JsonProperty("packageId") String packageId,
+                    @JsonProperty("results") ArrayList<TestResult> testResults) {
+                this.Id = packageId;
+                this.testResults = testResults;
+            }
+
+            public String getId() {
+                return this.Id;
+            }
+
+            public ArrayList<TestResult> getResult() {
+                return this.testResults;
+            }
+        }
+
     }
 }

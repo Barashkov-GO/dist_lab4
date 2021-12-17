451c388a5ebdef48bf06525b520b9e28937ce151
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-17 20:19:03 +0300

    Scripted auto-commit on change (2021-12-17 20:19:03) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/StorageActor.java
+++ src/main/java/ru.barashkov.distributed.lab4/StorageActor.java
@@ -10,7 +10,7 @@ public class StorageActor extends AbstractActor{
     private Map<Integer, ArrayList<TestResult>> storage = new HashMap<>();
     @Override
     public Receive createReceive() {
-        return ReceiveBuilder.create().match(MessageSetResult.class, setResult).match(MessageGetResult, )
+        return ReceiveBuilder.create().match(MessageSetResult.class, setResult).match(MessageGetResult, m -> )
     }
 
     private void setResult(MessageSetResult m) {

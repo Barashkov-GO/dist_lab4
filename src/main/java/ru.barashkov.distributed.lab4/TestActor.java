62987444f1b243ec0ca25f7bfa4c415676348ce7
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-17 23:58:06 +0300

    Scripted auto-commit on change (2021-12-17 23:58:06) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/TestActor.java
+++ src/main/java/ru.barashkov.distributed.lab4/TestActor.java
@@ -50,7 +50,8 @@ public class TestActor extends AbstractActor {
                             t.getTestName(),
                             t.getExpectedResult(),
                             executionResult
-                    )
+                    ),
+                    self()
             );
         }
     }

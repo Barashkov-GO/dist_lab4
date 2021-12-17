e04cb2e344dc91b2f80de511ac417c5a39f25506
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-18 00:02:16 +0300

    Scripted auto-commit on change (2021-12-18 00:02:16) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/ActorRouter.java
+++ src/main/java/ru.barashkov.distributed.lab4/ActorRouter.java
@@ -3,8 +3,9 @@ package ru.barashkov.distributed.lab4;
 import akka.actor.AbstractActor;
 
 public class ActorRouter extends AbstractActor {
+    private final 
     @Override
     public Receive createReceive() {
-        
+
     }
 }

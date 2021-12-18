9467fc7cb87397975d63b80977e440ffedfdf781
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-18 13:47:45 +0300

    Scripted auto-commit on change (2021-12-18 13:47:45) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/ActorRouter.java
+++ src/main/java/ru.barashkov.distributed.lab4/ActorRouter.java
@@ -22,7 +22,14 @@ public class ActorRouter extends AbstractActor {
         this.storageActorRef = getContext().actorOf(Props.create(ActorStorage.class));
         ArrayList<Routee> actors = new ArrayList<>();
         for (int i = 0; i < TEST_ACTORS_AMOUNT; i++) {
-            actors.add(new ActorRefRoutee(getContext().actorOf(Props.create(ActorTest.class))));
+            actors.add(
+                    new ActorRefRoutee(
+                            getContext().
+                                    actorOf(
+                                            Props.create(ActorTest.class)
+                                    )
+                    )
+            );
         }
         this.router = new Router(new BalancingRoutingLogic(), actors);
     }

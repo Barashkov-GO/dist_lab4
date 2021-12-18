46f6d522f2d85fcef3fe14578c5b0a4618931921
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-18 13:35:09 +0300

    Scripted auto-commit on change (2021-12-18 13:35:09) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab4/ActorRouter.java
+++ src/main/java/ru.barashkov.distributed.lab4/ActorRouter.java
@@ -21,7 +21,7 @@ public class ActorRouter extends AbstractActor {
         this.storageActorRef = getContext().actorOf(Props.create(ActorStorage.class));
         ArrayList<Routee> actors = new ArrayList<>();
         for (int i = 0; i < TEST_ACTORS_AMOUNT; i++) {
-            actors.add(getContext().actorOf(Props.create(ActorTest.class)));
+            actors.add((Routee) getContext().actorOf(Props.create(ActorTest.class)));
         }
         this.router = new Router(new BalancingRoutingLogic(), actors);
     }
--- src/main/java/ru.barashkov.distributed.lab4/App.java
+++ src/main/java/ru.barashkov.distributed.lab4/App.java
@@ -7,14 +7,18 @@ import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
-import akka.routing.BalancingPool;
-import akka.routing.RoundRobinPool;
+import akka.http.javadsl.model.StatusCodes;
+import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
-
-import java.util.concurrent.CompletionStage;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import scala.concurrent.Future;
 
 import static java.security.AccessController.getContext;
 
